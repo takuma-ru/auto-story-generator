8c6fe5d3bcb1a7dc226f28c7de323dc47b92ad60
takuma-ru <kanekotakuma@team-lab.com>
2024-01-18 17:23:24 +0900

    ➕ lint 系追加

--- packages/auto-story-generator/.prettierrc.cjs
+++ packages/auto-story-generator/.prettierrc.cjs
@@ -1,1 +1,13 @@
-module.exports = require("@vumdoc/prettier-config");
+/**
+ * @type {import('prettier').Config}
+ */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  bracketSameLine: false,
+  singleAttributePerLine: true,
+};
+
+module.exports = config;
