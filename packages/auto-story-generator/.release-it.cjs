7a67171cdb9909c5b3d9a4720d539ac6072164e9
takuma-ru <kanekotakuma@team-lab.com>
2024-02-29 16:08:21 +0900

    Add release branch creation and pull request creation

--- packages/auto-story-generator/.release-it.cjs
+++ packages/auto-story-generator/.release-it.cjs
@@ -11,9 +11,8 @@ module.exports = {
     commitMessage:
       ":bookmark: release @takuma-ru/auto-story-generator@${version}",
   },
-  hooks: {
-    "after:git:release":
-      "echo Release is done; git switch -c release/${version} ${version}; git push --set-upstream origin release/${version}; git switch main; git merge release/${version}; git push;",
-  },
+  /* hooks: {
+    "after:git:release": "git pull origin ${version}",
+  }, */
   plugins: {},
 };
