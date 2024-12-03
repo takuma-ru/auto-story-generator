124789b10b8c79717a7a478e0f36d676752e7793
takuma-ru <kanekotakuma@team-lab.com>
2024-12-03 17:05:20 +0900

    ✨ `@takuma-ru/rlse`

---
+++ packages/auto-story-generator/rlse.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "@takuma-ru/rlse";
+
+export default defineConfig({
+  name: "@takuma-ru/auto-story-generator",
+  buildCmd: "pnpm build",
+  gitUserName: "github-actions[bot]",
+  gitUserEmail: "41898282+github-actions[bot]@users.noreply.github.com",
+});
